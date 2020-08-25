02327a7045cfe044599ef65f922dac582ee72374
Mario Ciccarelli <kartone@macbookPro.homenet.telecomitalia.it>
2020-08-25 09:56:45 +0200

    First commit

--- get_malbazaar.py
+++ get_malbazaar.py
@@ -10,6 +10,11 @@ response = requests.post('https://mb-api.abuse.ch/api/v1/', data = data, timeout
 maldata = response.json()
 #print(json.dumps(maldata, indent=2, sort_keys=False))
 
-for key in maldata:
-    value = maldata[key]
+key=0
+while True:
+  try: 
+    value = maldata["data"][key]["sha256_hash"]
     print("The key and value are ({}) = ({})".format(key, value))
+    key+=1
+  except IndexError:
+    break
