b64629a9ba4797d5e566d63de10d0c725102a47f
Mario Ciccarelli <kartone@macbookPro.homenet.telecomitalia.it>
2020-08-25 11:51:07 +0200

    Changed the loop through the API results

--- get_malbazaar.py
+++ get_malbazaar.py
@@ -1,5 +1,4 @@
 # Testing malware bazaar API
-# Test
 
 import requests
 import json
@@ -10,11 +9,8 @@ response = requests.post('https://mb-api.abuse.ch/api/v1/', data = data, timeout
 maldata = response.json()
 #print(json.dumps(maldata, indent=2, sort_keys=False))
 
-key=0
-while True:
-  try: 
-    value = maldata["data"][key]["sha256_hash"]
-    print("The key and value are ({}) = ({})".format(key, value))
-    key+=1
-  except IndexError:
-    break
+for i in range(len(maldata["data"])):
+    for key in maldata["data"][i].keys():
+        if key == "sha256_hash":
+            value = maldata["data"][i][key]
+            print(key, "->", value)
